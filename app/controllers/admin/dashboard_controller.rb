905d3799998d348691fc895ffb32ea0b82e7f2bd
Jason Chen <windson7@gmail.com>
2025-07-31 21:59:03 -0700

    admin dashboard add category count and product count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class Admin::DashboardController < ApplicationController
+
   def show
+    @products = Product.all
+    @categories = Category.all
   end
+
+  def total_product_count
+    @products.map {|entry| entry[:quantity]}.sum
+  end
+  helper_method :total_product_count
+
+  def total_category_count
+    @categories.count
+  end
+  helper_method :total_category_count
 end
