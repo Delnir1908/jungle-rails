ac9d535575ef97eed87b9c62472809eba917d465
Jason Chen <windson7@gmail.com>
2025-07-31 21:13:03 -0700

    add basic http authenticate to admin page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with \
+  name: ENV["BASIC_AUTH_NAME"], 
+  password: ENV["BASIC_AUTH_PASSWORD"], 
+  only: [:index, :new, :create, :destroy]
+
   def index
     @products = Product.order(id: :desc).all
   end
